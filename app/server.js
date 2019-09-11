34ad7054958dc5e50b08414fd14007c69844955e
k-kuwahara <zensin0082@gmail.com>
2019-09-11 13:29:00 +0900

    feat: add boot file

---
+++ app/server.js
@@ -0,0 +1,45 @@
+const http = require('http')
+const cluster = require('cluster')
+const path = require('path')
+
+const port = process.env.PORT || '3000'
+const clusterNum = require('os').cpus().length * 2
+const app = require('./app')
+require('dotenv')
+  .config({ path: path.resolve(process.cwd(), `envs/.env.${process.env.DOT_ENV}`) })
+
+/**
+ * Event listener for HTTP server "error" event.
+ * @param {Error} err
+ */
+const onError = (err) => {
+  if (err.syscall !== 'listen') {
+    throw err
+  }
+  switch (err.code) {
+    case 'EACCES':
+      process.exit(1)
+      break
+
+    case 'EADDRINUSE':
+      process.exit(1)
+      break
+
+    default:
+      throw err
+  }
+}
+
+app.set('port', port)
+
+// Cluster
+if (cluster.isMaster) {
+  for (let i = 0; i < clusterNum; ++i) {
+    cluster.fork()
+  }
+} else {
+  const server = http.createServer(app)
+  server.listen(port)
+  console.log(`server is listening on ${port}...`)
+  server.on('error', onError)
+}
